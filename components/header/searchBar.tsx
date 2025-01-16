003f2c2f3a380a378b3d36876c1b5b4eba06ecae
Wahyu Esya <wahyu.esya17@gmail.com>
2025-01-16 13:20:51 +0700

    feat(SearchBar) add conditional search bar when in mobile

--- components/header/searchBar.tsx
+++ components/header/searchBar.tsx
@@ -1,18 +1,57 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { CircleX, Search } from "lucide-react";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [isOpenSearch, setIsOpenSearch] = useState(false);
+
+  const searchBtnClassName =
+    "button-padding cursor-pointer rounded-r-xl bg-primary hover:opacity-80";
+
   return (
-    <div className="flex w-3/4 lg:w-2/3">
-      <Input
-        type="text"
-        placeholder="Search"
-        className="h-10 rounded-r-none text-lg lg:h-14"
-        icon={Search}
-      />
-      <div className="flex-center button-padding cursor-pointer rounded-r-xl bg-primary hover:opacity-80">
-        <Search className="h-5 w-5 text-white" data-testid="search-icon" />
+    <div className="relative flex w-3/4 lg:w-2/3">
+      <div
+        className={cn(
+          "flex-1 sm:flex",
+          isOpenSearch
+            ? "fixed left-0 right-0 top-0 z-20 flex bg-white p-6"
+            : "hidden",
+        )}
+      >
+        {isOpenSearch && (
+          <div
+            className="flex-center mr-4"
+            onClick={() => setIsOpenSearch(false)}
+          >
+            <CircleX />
+          </div>
+        )}
+        <Input
+          type="text"
+          placeholder="Search"
+          className={cn(
+            "h-10 rounded-r-none text-lg sm:flex lg:h-14",
+            isOpenSearch ? "flex" : "hidden",
+          )}
+          icon={Search}
+        />
+        <button className={cn("flex-center", searchBtnClassName)}>
+          <Search className="h-5 w-5 text-white" data-testid="search-icon" />
+        </button>
       </div>
+      <button
+        className={cn(
+          "sm:hidden",
+          searchBtnClassName,
+          isOpenSearch ? "hidden" : "flex-center",
+        )}
+        onClick={() => setIsOpenSearch(true)}
+      >
+        <Search className="h-5 w-5 text-white" data-testid="search-icon" />
+      </button>
     </div>
   );
 };
