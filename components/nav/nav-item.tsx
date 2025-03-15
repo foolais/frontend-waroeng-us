53215988ec0d5cf9760e78543ef31c61c87e1e70
Wahyu Esya <wahyu.esya17@gmail.com>
2025-03-15 14:07:19 +0700

    fix(mobileSidebar) add store id in link url

--- components/nav/nav-item.tsx
+++ components/nav/nav-item.tsx
@@ -3,45 +3,55 @@
 import { adminNavItem } from "@/lib/constant";
 import { cn } from "@/lib/utils";
 import { iNavItem } from "@/types/types";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavItem = ({ isMobile = false }: { isMobile?: boolean }) => {
   const path = usePathname();
+  const { data: session } = useSession();
+  const storeId = session?.user?.store_id ?? "";
 
   return (
     <nav className={cn(isMobile ? "w-3/4" : "w-max lg:w-full")}>
       <ul className="mt-2 grid gap-2">
         {adminNavItem.map(
-          ({ title, icon: Icon, url }: iNavItem, index: number) => (
-            <li key={index}>
-              <Link
-                href={url}
-                aria-label={title}
-                className={cn(
-                  "flex items-center gap-4 rounded-xl p-2 transition",
-                  path?.startsWith(url)
-                    ? "bg-primary text-background hover:opacity-80"
-                    : "hover:bg-background hover:opacity-80",
-                  isMobile
-                    ? "justify-normal"
-                    : "justify-center lg:justify-normal",
-                )}
-              >
-                {Icon && (
-                  <Icon data-testid={`nav-icon-${title}`} className="h-5 w-5" />
-                )}
-                <span
+          ({ title, icon: Icon, url }: iNavItem, index: number) => {
+            const urlPath = `/${storeId}/${url}`;
+
+            return (
+              <li key={index}>
+                <Link
+                  href={urlPath}
+                  aria-label={title}
                   className={cn(
-                    "hidden text-sm font-semibold lg:block",
-                    isMobile ? "block" : "",
+                    "flex items-center gap-4 rounded-xl p-2 transition",
+                    path?.startsWith(urlPath)
+                      ? "bg-primary text-background hover:opacity-80"
+                      : "hover:bg-background hover:opacity-80",
+                    isMobile
+                      ? "justify-normal"
+                      : "justify-center lg:justify-normal",
                   )}
                 >
-                  {title}
-                </span>
-              </Link>
-            </li>
-          ),
+                  {Icon && (
+                    <Icon
+                      data-testid={`nav-icon-${title}`}
+                      className="h-5 w-5"
+                    />
+                  )}
+                  <span
+                    className={cn(
+                      "hidden text-sm font-semibold lg:block",
+                      isMobile ? "block" : "",
+                    )}
+                  >
+                    {title}
+                  </span>
+                </Link>
+              </li>
+            );
+          },
         )}
       </ul>
     </nav>
